bfcced723b986dd40be461a26b67181c14d96cdf
Pakkaphon Tangtonglang <117561370+Itsmeeins@users.noreply.github.com>
2025-03-12 13:07:21 +0700

    Update project_First.py

--- project_First.py
+++ project_First.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
-import folium
-from streamlit_folium import folium_static
+from streamlit_folium import folium_static,folium
 
 # Load Data
 def load_data():
